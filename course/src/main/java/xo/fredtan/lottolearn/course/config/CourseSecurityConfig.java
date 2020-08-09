dc6755a540d9396787df7908cebb778c65d5e06c
fredtanxo <fredtan4go@gmail.com>
2020-08-09 15:50:16 +0800

    course、user模块接入Spring Security

---
+++ course/src/main/java/xo/fredtan/lottolearn/course/config/CourseSecurityConfig.java
@@ -0,0 +1,22 @@
+package xo.fredtan.lottolearn.course.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@EnableWebSecurity
+public class CourseSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                    .antMatchers("/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
