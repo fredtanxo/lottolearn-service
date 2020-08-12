dbb5c4381f693d68c30cb0788c1195a844b26570
fredtanxo <fredtan4go@gmail.com>
2020-08-12 22:47:49 +0800

    发送心跳常量补充

--- api/src/main/java/xo/fredtan/lottolearn/api/message/constant/MessageConstants.java
+++ api/src/main/java/xo/fredtan/lottolearn/api/message/constant/MessageConstants.java
@@ -1,6 +1,10 @@
 package xo.fredtan.lottolearn.api.message.constant;
 
+import java.util.concurrent.TimeUnit;
+
 public class MessageConstants {
+    public static final Long HEARTBEAT_INCOMING = TimeUnit.SECONDS.toMillis(10L);
+    public static final Long HEARTBEAT_OUTGOING = TimeUnit.SECONDS.toMillis(10L);
     public static final String COURSE_ID = "course_id";
     public static final String LIVE_DISTRIBUTION_CHANNEL = "live_distribution";
 }
